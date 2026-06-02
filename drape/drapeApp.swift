61cd018d8cb0bda6c4de490d9623f46730b729d5
Joscha <sv5555r72t@privaterelay.appleid.com>
2026-06-02 15:54:38 +0200

    Initial Commit

---
+++ drape/drapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  drapeApp.swift
+//  drape
+//
+//  Created by Joscha Axthammer on 02.06.26.
+//
+
+import SwiftUI
+
+@main
+struct drapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
